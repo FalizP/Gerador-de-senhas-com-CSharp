d57e1913130bae3672f16f57b4a63663ec249b58
Luiz Palma <83788360+FalizP@users.noreply.github.com>
2021-11-01 14:08:47 -0300

    Update Program.cs

--- GeradordeSenhas/Program.cs
+++ GeradordeSenhas/Program.cs
@@ -18,7 +18,6 @@ namespace GeradordeSenhas
             Random random = new Random();
             StringBuilder password = new StringBuilder();
 
-
             for (int i = 0; i < len; i++)
             {
                 rnd = random.Next(0, pos.Length - 1);
